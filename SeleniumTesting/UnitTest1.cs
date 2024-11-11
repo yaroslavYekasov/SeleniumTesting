1e07ad445d444f53366f3d36c9776587ae300acb
opilane <opilane@ORDI-402602>
2024-11-11 11:15:18 +0200

    Final

--- SeleniumTesting/UnitTest1.cs
+++ SeleniumTesting/UnitTest1.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace SeleniumNUnitExample
@@ -22,49 +23,70 @@ namespace SeleniumNUnitExample
         }
 
         [Test]
-        public void AddMultipleProductsToCartAndOpenCart()
+        public void AutomateBuyingTenProducts()
         {
             driver.Navigate().GoToUrl("https://www.klick.ee/");
-
             Thread.Sleep(1000);
 
+            // cookies
             var allowAllButton = driver.FindElement(By.Id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"));
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", allowAllButton);
-
             Thread.Sleep(1000);
 
-            var productUrls = new List<string>
+            // to Telefonid ja Lisad
+            var telefonidLink = driver.FindElement(By.LinkText("Telefonid"));
+            telefonidLink.Click();
+            Thread.Sleep(2000);
+
+            // collect phones
+            var productElements = driver.FindElements(By.CssSelector(".product-link")).ToList();
+            if (productElements.Count == 0)
             {
-                "https://www.klick.ee/nutitelefon-samsung-galaxy-m15-5g-4-128gb",
-                "https://www.klick.ee/sulearvuti-dell-inspiron-3520-15-120hz-i5-16gb-1000gb-hobe",
-                "https://www.klick.ee/nutitelefon-honor-x6b-4-128gb",
-                "https://www.klick.ee/sulearvuti-lenovo-thinkpad-l13-gen-3-i5-16gb-256gb-must-1",
-                "https://www.klick.ee/nutitelefon-samsung-galaxy-a34-5g-6-128gb-must-enterprise-edition"
-            };
-
-            foreach (var url in productUrls)
-            {
-                driver.Navigate().GoToUrl(url);
-                Thread.Sleep(200);
-
-                try
-                {
-                    var buyButton = driver.FindElement(By.XPath("//button[contains(@class, 'add-to-cart') and contains(text(), 'Osta')]"));
-                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buyButton);
-                    Thread.Sleep(1000);
-                }
-                catch (NoSuchElementException)
-                {
-                    Assert.Fail($"The 'Osta' button could not be found or clicked for product: {url}");
-                }
-
-                driver.Navigate().GoToUrl("https://www.klick.ee/");
-
-                Thread.Sleep(200);
+                Assert.Fail("No products found on the page to add to the cart.");
             }
 
+            Random random = new Random();
+            HashSet<int> selectedIndexes = new HashSet<int>();
+
+
+            // add 3 random phones
+            while (selectedIndexes.Count < 3 && selectedIndexes.Count < productElements.Count)
+            {
+                int index = random.Next(productElements.Count);
+                if (!selectedIndexes.Contains(index))
+                {
+                    selectedIndexes.Add(index);
+
+                    var productLink = productElements[index];
+                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", productLink);
+                    Thread.Sleep(1000);
+
+                    ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", productLink);
+                    Thread.Sleep(1000);
+
+                    try
+                    {
+                        var buyButton = driver.FindElement(By.XPath("//button[contains(@class, 'add-to-cart') and contains(text(), 'Osta')]"));
+                        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buyButton);
+                        Thread.Sleep(1000);
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        Assert.Fail("The 'Add to Cart' button could not be found on the product page.");
+                    }
+
+                    driver.Navigate().Back();
+                    Thread.Sleep(1000);
+
+                    productElements = driver.FindElements(By.CssSelector(".product-link")).ToList();
+                }
+            }
+
+            // cart
             try
             {
+                ((IJavaScriptExecutor)driver).ExecuteScript("window.scrollTo(5, 5);");
+
                 var cartButton = driver.FindElement(By.CssSelector("button.relative.bg-cl-transparent.brdr-none.inline-flex.p10.icon.pointer"));
                 ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", cartButton);
                 Thread.Sleep(1000);
@@ -98,39 +120,38 @@ namespace SeleniumNUnitExample
                 lastNameInput.SendKeys("Testorro");
 
                 var emailInput = driver.FindElement(By.Name("email-address"));
-                emailInput.SendKeys("Tesro@gmail.com");
+                emailInput.SendKeys("Testro@gmail.com");
 
                 var nextButton = driver.FindElement(By.Id("Kliendi_info"));
                 ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", nextButton);
-                Thread.Sleep(5000);
+                Thread.Sleep(1000);
             }
             catch (NoSuchElementException)
             {
                 Assert.Fail("Could not find an element in the checkout form.");
             }
 
-            // Click on the dropdown to open it
+            // dropdown 
             var dropdown = driver.FindElement(By.XPath("//select[@name='faststorepickup']"));
             dropdown.Click();
-            Thread.Sleep(5000); 
+            Thread.Sleep(1000);
 
-            // Select the desired option by visible text
+            // select option 
             var option = driver.FindElement(By.XPath("//option[contains(text(), 'Jõhvi - Pargi Keskus')]"));
             option.Click();
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var phoneInput = driver.FindElement(By.XPath("//input[@name='phone-number']"));
             phoneInput.SendKeys("54574479");
 
             var proceedButton = driver.FindElement(By.XPath("//button[@data-testid='shippingSubmit']"));
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", proceedButton);
-            Thread.Sleep(10000);
+            Thread.Sleep(1000);
         }
 
         [OneTimeTearDown]
         public void Teardown()
         {
-            driver?.Quit();
             driver?.Quit();
             driver?.Dispose();
         }
